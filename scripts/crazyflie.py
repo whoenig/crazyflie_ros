af44384893e373b051ef18019b1d9c220e5b6b47
Wolfang Hoenig <whoenig@usc.edu>
2014-10-19 20:54:01 -0700

    Improved robustness for missing dongle or disconnections (auto-reconnect)

--- scripts/crazyflie.py
+++ scripts/crazyflie.py
@@ -7,11 +7,13 @@ import time, sys
 from threading import Thread
 
 class CrazyflieROS:
-    """Example that connects to a Crazyflie and ramps the motors up/down and
-    the disconnects"""
-    def __init__(self, link_uri):
-        """ Initialize and run the example with the specified link_uri """
+    Disconnected = 0
+    Connecting = 1
+    Connected = 2
 
+    """Wrapper between ROS and Crazyflie SDK"""
+    def __init__(self, link_uri):
+        self.link_uri = link_uri
         self._cf = Crazyflie()
 
         self._cf.connected.add_callback(self._connected)
@@ -19,52 +21,64 @@ class CrazyflieROS:
         self._cf.connection_failed.add_callback(self._connection_failed)
         self._cf.connection_lost.add_callback(self._connection_lost)
 
-        self._cf.open_link(link_uri)
-        rospy.loginfo("Connectving to %s" % link_uri)
-
         self._cmdVel = Twist()
         rospy.Subscriber("cmd_vel", Twist, self._cmdVelChanged)
+
+        self._state = CrazyflieROS.Disconnected
+        Thread(target=self._update).start()
+
+    def _try_to_connect(self):
+        rospy.loginfo("Connecting to %s" % self.link_uri)
+        self._state = CrazyflieROS.Connecting
+        self._cf.open_link(self.link_uri)
 
     def _connected(self, link_uri):
         """ This callback is called form the Crazyflie API when a Crazyflie
         has been connected and the TOCs have been downloaded."""
 
-        # Start a separate thread to do the motor test.
-        # Do not hijack the calling thread!
-        #Thread(target=self._update).start()
+        rospy.loginfo("Connected to %s" % link_uri)
+        self._state = CrazyflieROS.Connected
 
     def _connection_failed(self, link_uri, msg):
         """Callback when connection initial connection fails (i.e no Crazyflie
         at the speficied address)"""
         rospy.logfatal("Connection to %s failed: %s" % (link_uri, msg))
+        self._state = CrazyflieROS.Disconnected
 
     def _connection_lost(self, link_uri, msg):
         """Callback when disconnected after a connection has been made (i.e
         Crazyflie moves out of range)"""
         rospy.logfatal("Connection to %s lost: %s" % (link_uri, msg))
+        self._state = CrazyflieROS.Disconnected
 
     def _disconnected(self, link_uri):
         """Callback when the Crazyflie is disconnected (called in all cases)"""
         rospy.logfatal("Disconnected from %s" % link_uri)
+        self._state = CrazyflieROS.Disconnected
 
-    def _cmdVelChanged(self, data):
-        self._cmdVel = data
+    def _send_setpoint(self):
         roll = self._cmdVel.linear.y
         pitch = self._cmdVel.linear.x
         yawrate = self._cmdVel.angular.z
         thrust = max(10000, int(self._cmdVel.linear.z))
-        print(roll, pitch, yawrate, thrust)
+        #print(roll, pitch, yawrate, thrust)
         self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+
+    def _cmdVelChanged(self, data):
+        self._cmdVel = data
+        self._send_setpoint()
 
     def _update(self):
         while not rospy.is_shutdown():
-            roll = self._cmdVel.linear.y
-            pitch = self._cmdVel.linear.x
-            yawrate = self._cmdVel.angular.z
-            thrust = max(10000, int(self._cmdVel.linear.z))
-            print(roll, pitch, yawrate, thrust)
-            #self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
-            rospy.sleep(0.2)
+            if self._state == CrazyflieROS.Disconnected:
+                self._try_to_connect()
+            elif self._state == CrazyflieROS.Connected:
+                # Crazyflie will shut down if we don't send any command for 500ms
+                # Hence, make sure that we don't wait too long
+                self._send_setpoint()
+                rospy.sleep(0.2)
+            else:
+                rospy.sleep(0.5)
         self._cf.commander.send_setpoint(0, 0, 0, 0)
         # Make sure that the last packet leaves before the link is closed
         # since the message queue is not flushed before closing
