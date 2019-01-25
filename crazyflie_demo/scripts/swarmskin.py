b11850a06174ffe1ef40f694f8404d87ccbc1d7f
negvet <e.tsykunov@gmail.com>
2019-01-25 16:38:45 +0300

    update swarmskin

--- crazyflie_demo/scripts/swarmskin.py
+++ crazyflie_demo/scripts/swarmskin.py
@@ -12,78 +12,75 @@ import os
 from multiprocessing import Process
 import random
 
-
-
-rospy.init_node('test_high_level')
-
-
-cf1_name = 'cf1'
-lp2_name = 'lp2' # lp1
-
-drone1 = swarmlib.Drone(cf1_name)
-lp2    = swarmlib.Mocap_object(lp2_name)
-
 def start_recording():
     os.system("rosbag record -o Desktop/SwarmSkin/ /vicon/crazyflie100/crazyflie100 /vicon/landing_pad/landing_pad")
-
-pose_recording = Process(target=start_recording)
-
-
-
-def land_detector(cf):
-
+def land_detector():
     while not rospy.is_shutdown():
-
         drone1.position()
-        lp2.position()
-
-        print "abs(drone1.pose[2] - lp2.pose[2])", abs(drone1.pose[2] - lp2.pose[2])
-
-        if abs(drone1.pose[2] - lp2.pose[2])<0.05:
+        lp1.position()
+        # lp2.position()
+        if abs(drone1.pose[2] - lp1.pose[2])<0.05:
             print "Stop motors"
-            cf.stop()
-            # cf.setParam("ring/effect", 14)
+            cf1.stop()
+            # cf1.setParam("ring/effect", 14)
             time.sleep(0.1)
-
-            # print 'kill the recorder'
-            # node_list = os.popen("rosnode list").read()
-            # os.system('rosnode kill '+node_list[72:99])
-
+            if data_recording:
+                print 'kill the recorder'
+                node_list = os.popen("rosnode list").read()
+                os.system('rosnode kill '+node_list[72:99])
             print "Shutdown"
             rospy.signal_shutdown("landed")
 
 
+rospy.init_node('test_high_level')
 
+# Names and variables
+TAKEOFFHEIGHT = 1.8
+data_recording = False
+cf1_name = 'cf1'
+lp1_name = 'lp1'
+lp2_name = 'lp2' # lp1
 
+drone1 = swarmlib.Drone(cf1_name)
+drone_list = [drone1]
+lp1    = swarmlib.Mocap_object(lp1_name)
+lp2    = swarmlib.Mocap_object(lp2_name)
+lp_list = [lp1, lp2]
 
-
-
-
-
-cf = crazyflie.Crazyflie("cf1", "/vicon/cf1/cf1")
-
-
+cf1 = crazyflie.Crazyflie(cf1_name, '/vicon/'+cf1_name+'/'+cf1_name)
+cf_list = [cf1]
 
 # landing_velocity = random.choice([13,22]) #13,22
 # landing_velocity = 13
-landing_velocity = 22
+# landing_velocity = 22
+landing_velocity = 30
 print "landing_velocity", landing_velocity
 
 
 
-cf.setParam("commander/enHighLevel", 1)
-cf.setParam("stabilizer/estimator", 2) # Use EKF
-cf.setParam("stabilizer/controller", 2) # Use mellinger controller
-cf.takeoff(targetHeight = 2.0, duration = 7.0)
+
+
+print "Takeoff"
+for cf in cf_list:
+    cf.setParam("commander/enHighLevel", 1)
+    cf.setParam("stabilizer/estimator", 2) # Use EKF
+    cf.setParam("stabilizer/controller", 2) # Use mellinger controller
+    cf.takeoff(targetHeight = TAKEOFFHEIGHT, duration = 7.0)
 time.sleep(7.0)
 
-# pose_recording.start()
 
 
+if data_recording:
+    print "Data recording started"
+    pose_recording = Process(target=start_recording)
+    pose_recording.start()
 
-cf.land(targetHeight = 0.0, duration = landing_velocity)
+print 'Landing...'
+for cf in cf_list:
+    cf.land(targetHeight = 0.0, duration = landing_velocity)
 
-land_detector(cf)
+
+land_detector()
 
 
 
